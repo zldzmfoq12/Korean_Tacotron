4aeb00a54c297a2ad4571ca05acb04a4b2ab7687
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-02-22 18:13:56 +0900

    Update tacotron.py

--- models/tacotron.py
+++ models/tacotron.py
@@ -75,7 +75,7 @@ class Tacotron2():
             c_encoder_outputs = tf.concat(c_outputs, axis=2) # Concat and return forward + backward outputs
             # p_envoder_outpust = [N,p_T,2*encoder_lstm_units] = [N,p_T,512]
             p_encoder_outputs = tf.concat(p_outputs, axis=2)
-            diff = c_encoder_outputs.shape[1]-p_encoder_outputs.shape[1]
+            diff = c_input_lengths-p_input_lengths
             paddings = tf.constant([[0, 0], [0, diff], [0,0]])
             p_encoder_outputs = tf.pad(p_encoder_outputs, paddings, "CONSTANT") 
             # Concat and return character + phoneme = [N, c_T+p_T, 512]
