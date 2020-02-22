758c75d3d140eba4d0eb9c9f037c4a733d3cb9d0
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-02-22 19:28:09 +0900

    Update train2.py

--- train2.py
+++ train2.py
@@ -62,6 +62,7 @@ def train(log_dir, args):
 	with tf.variable_scope('model') as scope:
 		model = create_model(args.model, hparams)
 		diff = feeder.c_input_lengths-feeder.p_input_lengths
+		print(diff)
 		p_inputs= tf.pad(feeder.p_inputs, [[0, 0,], [0, diff]], "CONSTANT")
 		model.initialize(feeder.c_inputs, p_inputs, feeder.c_input_lengths, feeder.c_input_lengths, feeder.mel_targets, feeder.linear_targets)
 		model.add_loss()
