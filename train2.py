b0b3a203d2553e708855f1b0ea74b492cae8158f
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-02-22 19:37:04 +0900

    Update train2.py

--- train2.py
+++ train2.py
@@ -61,9 +61,9 @@ def train(log_dir, args):
 	global_step = tf.Variable(0, name='global_step', trainable=False)
 	with tf.variable_scope('model') as scope:
 		model = create_model(args.model, hparams)
-		diff = tf.to_int32(feeder.c_input_lengths)-tf.to_int32(feeder.p_input_lengths)
-		print(tf.to_int32(diff))
-		p_inputs= tf.pad(feeder.p_inputs, [[0, 0,], [0, tf.to_int32(diff)]], "CONSTANT")
+		diff = feeder.c_input_lengths-feeder.p_input_lengths
+		print(diff.numpy())
+		p_inputs= tf.pad(feeder.p_inputs, [[0, 0,], [0, diff.numpy()]], "CONSTANT")
 		model.initialize(feeder.c_inputs, p_inputs, feeder.c_input_lengths, feeder.c_input_lengths, feeder.mel_targets, feeder.linear_targets)
 		model.add_loss()
 		model.add_optimizer(global_step)
