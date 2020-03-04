5a17a4d48aad120d08d4e4b712fd8311353712c5
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-03-04 15:23:32 +0900

    [SH] path modified

--- preprocess.py
+++ preprocess.py
@@ -51,7 +51,7 @@ def write_metadata(metadata, out_dir):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_dir', default=os.path.expanduser('~/jeewoo/Korean_Tacotron/'))
+    parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron/Korean_Tacotron/'))
     parser.add_argument('--output', default='training')
     parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech', 'bible', 'kss'])
     parser.add_argument('--num_workers', type=int, default=cpu_count())
