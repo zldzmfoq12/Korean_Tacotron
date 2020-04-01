1a36735824812099f180bbb4523dd64907c572ce
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-04-01 17:15:20 +0900

    Update eval.py

--- eval.py
+++ eval.py
@@ -5,17 +5,16 @@ from hparams import hparams, hparams_debug_string
 from synthesizer import Synthesizer
 
 
-# sentences = [
-#   # 장기하와 얼굴들 ㅋ 가사:
-#   '신진 샹숑가수의 신춘 샹숑쇼우.',
-#   '철수 책상 철 책상.',
-#   '창경원 창살은 쌍창살',
-#   '스위스에서 온 스미스씨',
-#   # 장기하와 얼굴들 새해복 가사:
+sentences = [
+  # 장기하와 얼굴들 ㅋ 가사:
+  '나는 대체로 선생님의 말씀에 귀를 기울이지 않았다',
+  '나는 눈썹은 까만데 머리는 갈색이요',
+  '제가 처음 남편을 만났을 때 남편은 대머리가 아니었어요'
+  # 장기하와 얼굴들 새해복 가사:
 #   '간장 공장 공장장',
 #   '한양양장점 옆 한양양장점',
 #   '후회한 시간을 후회할 거잖아',
-# ]
+]
 
 
 def get_output_base_path(checkpoint_path):
@@ -30,16 +29,16 @@ def run_eval(args):
     synth = Synthesizer()
     synth.load(args.checkpoint)
     base_path = get_output_base_path(args.checkpoint)
-    sentences=[]
-    with open('./eval_concat.txt', encoding='utf-8') as f:
-        for line in f:
-            try:
-                parts = line.strip().replace('"', '').split('|')
-                c_text = parts[3]
-                p_text = parts[4]
-                sentences.append(c_text+'|'+p_text)
-            except:
-                pass
+    # sentences=[]
+    # with open('./eval_concat.txt', encoding='utf-8') as f:
+    #     for line in f:
+    #         try:
+    #             parts = line.strip().replace('"', '').split('|')
+    #             c_text = parts[3]
+    #             p_text = parts[4]
+    #             sentences.append(c_text+'|'+p_text)
+    #         except:
+    #             pass
     for i, text in enumerate(sentences):
         path = '%s-%d.wav' % (base_path, i)
         print('Synthesizing: %s' % path)
